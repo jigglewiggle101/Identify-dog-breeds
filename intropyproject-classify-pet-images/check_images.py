e3d8f85febc6ae4376c436ff3d09243137f95a6b
Jigglewiggle101 <s10262431@connect.np.edu.sg>
2024-12-01 15:39:46 +0800

    updated all necessary changes...

--- intropyproject-classify-pet-images/check_images.py
+++ intropyproject-classify-pet-images/check_images.py
@@ -2,21 +2,22 @@
 # -*- coding: utf-8 -*-
 # */AIPND-revision/intropyproject-classify-pet-images/check_images.py
 #
-# PROGRAMMER: [Your Name]
-# DATE CREATED: [Date]
-# REVISED DATE: [Date]
+# PROGRAMMER: Joshua Gamboa Calvo
+# DATE CREATED: 1/11/2024
+# REVISED DATE: [Insert Date]
 # PURPOSE: Classifies pet images using a pretrained CNN model, compares these
-#          classifications to the true identity of the pets in the images, and
-#          summarizes how well the CNN performed on the image classification task.
+#          classifications to the true identity of the pets in the images, 
+#          and summarizes the model's performance. The program compares 3 CNN 
+#          architectures to find the best model.
 #
-# Use argparse Expected Call with <> indicating expected user input:
+# Use argparse for user input:
 #      python check_images.py --dir <directory with images> --arch <model>
 #             --dogfile <file that contains dognames>
 #   Example call:
 #    python check_images_solution.py --dir pet_images/ --arch vgg --dogfile dognames.txt
 ##
 
-# Imports python modules
+# Imports
 from time import time
 from print_functions_for_lab_checks import *
 from get_input_args import get_input_args
@@ -26,54 +27,41 @@ from adjust_results4_isadog import adjust_results4_isadog
 from calculates_results_stats import calculates_results_stats
 from print_results import print_results
 
-# Main program function defined below
+# Main program function
 def main():
-    # Measures total program runtime by collecting start time
     start_time = time()
-    
-    # Retrieves Command Line Arguments from user input
     in_arg = get_input_args()
     
-    # Function that checks command line arguments using in_arg
+    # Check command line arguments
     check_command_line_arguments(in_arg)
     
-    # Creates pet labels dictionary using get_pet_labels function
+    # Create pet labels dictionary
     results = get_pet_labels(in_arg.dir)
-    
-    # Function that checks Pet Images in the results Dictionary
     check_creating_pet_image_labels(results)
     
-    # Creates Classifier Labels with classifier function and compares them
+    # Classify images and compare labels
     classify_images(in_arg.dir, results, in_arg.arch)
-    
-    # Function that checks Results Dictionary for classification
     check_classifying_images(results)
     
-    # Adjusts results to determine if image labels are correctly classified as dogs
+    # Adjust results for dog classification
     adjust_results4_isadog(results, in_arg.dogfile)
-    
-    # Function that checks Results Dictionary for 'is-a-dog' adjustment
     check_classifying_labels_as_dogs(results)
     
-    # Calculates results statistics and creates results_stats dictionary
+    # Calculate and check results statistics
     results_stats = calculates_results_stats(results)
-    
-    # Function that checks Results Statistics Dictionary
     check_calculating_results(results, results_stats)
     
-    # Prints summary results, incorrect classifications of dogs, and incorrect breeds
+    # Print results
     print_results(results, results_stats, in_arg.arch, True, True)
     
-    # Measures total program runtime by collecting end time
+    # Measure total program runtime
     end_time = time()
-    
-    # Computes overall runtime and prints it in hh:mm:ss format
     tot_time = end_time - start_time
     print("\n** Total Elapsed Runtime:",
-          str(int((tot_time/3600)))+":"+str(int((tot_time%3600)/60))+":"
-          +str(int((tot_time%3600)%60)))
+          str(int((tot_time / 3600))) + ":" + str(int((tot_time % 3600) / 60)) + ":"
+          + str(int((tot_time % 3600) % 60)))
 
 
-# Call to main function to run the program
+# Call main function
 if __name__ == "__main__":
     main()
